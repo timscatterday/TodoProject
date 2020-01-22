cb654f8300eb1f5c8e86e11ab283d0dde7b97471
Tim Scatterday <timothyscatterday@outlook.com>
2020-01-22 15:57:10 -0800

    First commit

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore;
+    window.store = store;
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>TODO App</h1>, root);
+});
